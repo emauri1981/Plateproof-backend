03d7b1d01f870b70743e6ace5172f9b24f96cea5
emauri1981 <emauri1981@gmail.com>
2025-11-13 13:43:30 -0500

    Update user.entity.ts

--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -1,5 +1,17 @@
+@Entity()
 export class User {
-  id: string;
-  email: string;
-  password: string;
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column()
+  name!: string;
+
+  @Column({ unique: true })
+  email!: string;
+
+  @Column()
+  password!: string;
+
+  @Column({ default: 'user' })
+  role!: string;
 }
